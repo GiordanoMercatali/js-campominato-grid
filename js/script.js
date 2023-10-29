a3ddf55ee4ecf8df06bb80db6d776a252d2e87da
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-29 19:56:53 +0100

    fixed: cells size changes based on difficulty

--- js/script.js
+++ js/script.js
@@ -17,24 +17,50 @@ startBtn.addEventListener("click", function(){
 
     if (difficulty === "easy"){
         cellNumber = 100;
-    } else if (difficulty === "medium"){
-        cellNumber = 81;
-    } else{
-        cellNumber = 49;
-    }
-
-    for (let i = 0; i < cellNumber; i++){
+        for (let i = 0; i < cellNumber; i++){
         numberArray[i] = i + 1;
         curNumber = numberArray[i];
-        // console.log(i, curNumber);
-        const cell = generateGridCell(curNumber);
+        const cell = generateGridCellEasy(curNumber);
         cell.addEventListener("click", handleCellClick);
         gridElem.append(cell);
     }
+    } else if (difficulty === "medium"){
+        cellNumber = 81;
+        for (let i = 0; i < cellNumber; i++){
+        numberArray[i] = i + 1;
+        curNumber = numberArray[i];
+        const cell = generateGridCellMedium(curNumber);
+        cell.addEventListener("click", handleCellClick);
+        gridElem.append(cell);
+    }
+    } else{
+        cellNumber = 49;
+        for (let i = 0; i < cellNumber; i++){
+        numberArray[i] = i + 1;
+        curNumber = numberArray[i];
+        const cell = generateGridCellHard(curNumber);
+        cell.addEventListener("click", handleCellClick);
+        gridElem.append(cell);
+    }
+    }
 
-    function generateGridCell(innerNumber){
+    function generateGridCellEasy(innerNumber){
         const newCell = document.createElement("div");
         newCell.classList.add("cell");
+        newCell.innerHTML = innerNumber;
+        return newCell;
+    }
+
+    function generateGridCellMedium(innerNumber){
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell", "small");
+        newCell.innerHTML = innerNumber;
+        return newCell;
+    }
+
+    function generateGridCellHard(innerNumber){
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell", "big");
         newCell.innerHTML = innerNumber;
         return newCell;
     }
